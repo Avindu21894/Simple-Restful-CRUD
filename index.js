6894e1c1c625d3472134ad296cbdaaee8b594976
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-12-28 20:43:09 +0530

    allow enter products using form format in postman

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const Product = require("./models/product.model");
 app.use(express.json());
+app.use(express.urlencoded({extended: false})); // Allows to add using form format in API (Postman)
 
 mongoose
   .connect(
